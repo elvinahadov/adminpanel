0c47a8a59d67e26cedcac45256056b03db88b2d7
Elvin Ahadov <elvineah@code.edu.az>
2024-09-09 17:51:05 +0400

    changes

--- src/components/AddProductPage.jsx
+++ src/components/AddProductPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AddProductPage = () => {
   return (
-    <div>
+    <div className="bg-gray-800 h-[100vh] my-2 w-full">
       
     </div>
   )
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Navbar from "./Navbar";
 import UsersPage from "./UsersPage";
 import ProductsPage from "./ProductsPage";
+import AddUserPage from "./AddUserPage"
 
 const Dashboard = ({ sidebarToggle, setSidebarToggle, selected }) => {
   return (
-    <div className={`${sidebarToggle ? "" : " ml-64 "} w-full`}>
+    <div className={`${sidebarToggle ? "" : " ml-64 "} w-full bg-gray-900`}>
       <Navbar
         sidebarToggle={sidebarToggle}
         setSidebarToggle={setSidebarToggle}
       />
-      {selected === "users" ? <UsersPage /> : ""}
-      {selected === "products" ? <ProductsPage /> : ""}
       
+      {selected === "users" ? <UsersPage /> : ""}
+      {selected === "products" ? <ProductsPage /> : ""}   
+      {selected === "addUser" ?<AddUserPage/>:""}
     </div>
   );
 };
